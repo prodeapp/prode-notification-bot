0297753486656145f77fd764007da04797c4ffdb
Algozino Santiago <santiago.algozino@satellogic.com>
2022-09-10 21:08:26 -0300

    change main to use database

--- main.py
+++ main.py
@@ -4,27 +4,20 @@ from datetime import datetime
 from dotenv import load_dotenv
 
 from graphql import getNewAnswers, getNewMarkets
-from database import create_table, read_last_timestamp, write_last_timestamp
+from database import read_last_timestamp, write_last_timestamp
 
 
 def main():
-    with open('timestamps.json', 'r+') as f:
-        timestamps = json.load(f)
-        print("last timestamp that I've checked: {}"
-              .format(datetime.fromtimestamp(
-                  int(timestamps['last_timestamp']))))
-        getNewAnswers(timestamps['last_timestamp'])
-        getNewMarkets(timestamps['last_timestamp'])
-        timestamps['last_timestamp'] = datetime.now().timestamp()
-        # go to the beggining of the file
-        f.seek(0)
-        json.dump(timestamps, f)
+
+    timestamps = read_last_timestamp()
+    print("last timestamp that I've checked: {}"
+          .format(datetime.fromtimestamp(
+              int(timestamps['last_timestamp']))))
+    getNewAnswers(timestamps['last_timestamp'])
+    getNewMarkets(timestamps['last_timestamp'])
+    write_last_timestamp(int(datetime.now().timestamp()))
 
 
 if __name__ == '__main__':
     load_dotenv()
-    # main()
-
-    # create_table()
-    print(read_last_timestamp())
-    write_last_timestamp(int(datetime.timestamp(datetime.now())))
+    main()
